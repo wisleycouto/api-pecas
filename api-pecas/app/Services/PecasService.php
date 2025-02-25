02cae28e53fb1a6559ec1c60ee52eab885c67179
Guilherme Ferreira Brandao (SE/STIC/CGSA) <guilhermefbrandao@mec.gov.br>
2025-02-25 08:19:37 -0300

    refinamentoController

--- api-pecas/app/Services/PecasService.php
+++ api-pecas/app/Services/PecasService.php
@@ -11,9 +11,37 @@ class PecasService
 
     public function cadastrarPeca(array $data)
     {
-        return Pecas::create($data);
+        $validatedData = validator($data, [
+            'nome_peca' => 'required|string',
+            'descricao_peca' => 'required|string',
+            'disponibilidade_peca' => 'required|int',
+            'preco_peca' => 'required|int',
+            'imposto' => 'int',
+            'co_fabricante' => 'required|string'
+        ])->validate();
+
+        return Pecas::create($validatedData);
     }
 
+    public function atualizaPeca(array $data, $co_peca)
+    {
+        $validatedData = validator($data, [
+            'nome_peca' => 'sometimes|required|string',
+            'descricao_peca' => 'sometimes|required|string',
+            'disponibilidade_peca' => 'sometimes|required|int',
+            'preco_peca' => 'sometimes|required|int',
+            'imposto' => 'sometimes|int',
+            'co_fabricante' => 'sometimes|required|string'
+        ])->validate();
+
+        $peca = Pecas::find($co_peca);
+        if ($peca) {
+            $peca->update($validatedData);
+            return $peca;
+        } else {
+            throw new \Exception('Peça não encontrada', 404);
+        }
+    }
 
 
 
